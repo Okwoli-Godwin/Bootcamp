ca1ce8b90e81f5f7c61d97625522dea7293e3cc2
abrahamk29@gmail.com <abrahamk29@gmail.com>
2023-03-16 19:14:06 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
-import ContactUs from './Components/ContactUs/ContactUs'
+// import ContactUs from './Components/ContactUs/ContactUs'
 import Allroutes from './Components/Allroutes'
-import Products from './Components/Products/Product'
-import Signin from './Components/Signin/Signin'
-import Signup from './Components/Signup/Signup'
+// import Products from './Components/Blog/Blog'
+// import Signin from './Components/Signin/Signin'
+// import Signup from './Components/Signup/Signup'
+// import TalkToUs from './Components/ContactUs/TalkToUs'
+// import Blog from './Components/Blog/Blog'
 
 const App = () => {
   return (
     <div>
       <Allroutes />
-      {/* <Products />
+      {/* <Blog />
       <br /><br />
       <Signup />
       <br /> <br />
       <ContactUs />
-      <br /> <br /> <br />
-      <Signin /> */}
+      <TalkToUs />
+      <br /> <br /> <br /> */}
+      {/* <Signin /> */}
     </div>
   )
 }
